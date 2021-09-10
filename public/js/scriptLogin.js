c0b0b9b4aa193779599d7d9c15bae710fc1a66dd
Emanuelly Maria Ogg Vicente <emanuelly.vicente@capgemini.com>
2021-09-10 10:37:24 -0300

    feat: criado chamada tipo post para rota login

---
+++ public/js/scriptLogin.js
@@ -0,0 +1,27 @@
+var btnLogin = document.getElementById("btnLogin");
+btnLogin.addEventListener("click", function () {
+
+  var usuario = document.getElementById("idNameUser").value;
+  var password = Number(document.getElementById("idPassword").value);
+
+  const update = {
+    userId: usuario,
+    senha: password
+  }
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(update),
+  };
+
+  let response = await verificarUser(options);
+  console.log(response);
+});
+
+async function verificarUser(options) {
+  let response = await fetch("http://localhost:3001/login", options);
+  return response.json();
+}
